2eb3a6cae8ef5f7735954d79a659da226a024bec
AKraja777 <ashokkumar7hitter@gmail.com>
2023-08-18 10:26:04 +0530

    edit-user-form.php

--- public/edit-user-form.php
+++ public/edit-user-form.php
@@ -63,6 +63,8 @@ if (isset($_POST['btnEdit'])) {
             $project_type = $db->escapeString(($_POST['project_type']));
             $today_mails = $db->escapeString(($_POST['today_mails']));
             $total_mails = $db->escapeString(($_POST['total_mails']));
+            $current_refers = $db->escapeString(($_POST['current_refers']));
+            $target_refers = $db->escapeString(($_POST['target_refers']));
             $error = array();
 
             if (empty($lead_id)) {
@@ -251,7 +253,7 @@ if (isset($_POST['btnEdit'])) {
 
         }
     
-        $sql_query = "UPDATE users SET name='$name', mobile='$mobile', password='$password', dob='$dob', email='$email', city='$city', refer_code='$refer_code', referred_by='$referred_by', earn='$earn', balance='$balance', withdrawal_status=$withdrawal_status,total_codes=$total_codes, today_codes=$today_codes,device_id='$device_id',status = $status,code_generate = $code_generate,code_generate_time = $code_generate_time,joined_date = '$joined_date',mcg_timer='$mcg_timer',security='$security',black_box='$black_box',salary_advance_balance='$salary_advance_balance',duration='$duration',worked_days='$worked_days',lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',trial_wallet='$trial_wallet',per_code_cost=$per_code_cost,plan=$plan,num_sync_times=$num_sync_times,l_referral_count=$l_referral_count,sa_withdrawal=$sa_withdrawal,level=$level,per_code_val=$per_code_val,earnings_wallet=$earnings_wallet,bonus_wallet=$bonus_wallet,project_type='$project_type' ,today_mails=$today_mails,total_mails=$total_mails WHERE id =  $ID";
+        $sql_query = "UPDATE users SET name='$name', mobile='$mobile', password='$password', dob='$dob', email='$email', city='$city', refer_code='$refer_code', referred_by='$referred_by', earn='$earn', balance='$balance', withdrawal_status=$withdrawal_status,total_codes=$total_codes, today_codes=$today_codes,device_id='$device_id',status = $status,code_generate = $code_generate,code_generate_time = $code_generate_time,joined_date = '$joined_date',mcg_timer='$mcg_timer',security='$security',black_box='$black_box',salary_advance_balance='$salary_advance_balance',duration='$duration',worked_days='$worked_days',lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',trial_wallet='$trial_wallet',per_code_cost=$per_code_cost,plan=$plan,num_sync_times=$num_sync_times,l_referral_count=$l_referral_count,sa_withdrawal=$sa_withdrawal,level=$level,per_code_val=$per_code_val,earnings_wallet=$earnings_wallet,bonus_wallet=$bonus_wallet,project_type='$project_type' ,today_mails=$today_mails,total_mails=$total_mails,current_refers=$current_refers,target_refers=$target_refers  WHERE id =  $ID";
         $db->sql($sql_query);
         $update_result = $db->getResult();
         if (!empty($update_result)) {
@@ -535,6 +537,19 @@ if (isset($_POST['btnCancel'])) { ?>
                                      </div>
      
                                   </div>
+                                </div>
+                                <br>
+                                <div class="row">   
+                                  <div class="form-group">
+                                     <div class="col-md-3">
+                                         <label for="exampleInputEmail1">Current Refers</label><i class="text-danger asterik">*</i>
+                                           <input type="number" class="form-control" name="current_refers" value="<?php echo $res[0]['current_refers']; ?>">
+                                             </div>
+                                     <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Target Refers</label><i class="text-danger asterik">*</i>
+                                            <input type="number" class="form-control" name="target_refers" value="<?php echo $res[0]['target_refers']; ?>">
+                                           </div>
+                                       </div>
                                 </div>
                         <br>
                         <div class="row">
