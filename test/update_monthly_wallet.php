2726bef36a26ab2ca0b2737d2dc03b26b124236c
jayaprasad356 <jayaprasad356@gmail.com>
2023-09-26 23:15:13 +0530

    git

--- test/update_monthly_wallet.php
+++ test/update_monthly_wallet.php
@@ -14,41 +14,33 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
-$sql = "SELECT id FROM `users` WHERE project_type = 'abcd' AND worked_days > duration AND code_generate = 1 AND status = 1";
+$datetime = date('Y-m-d H:i:s');
+$sql = "SELECT id,refer_code FROM users WHERE current_refers > 0 AND  current_refers < 4 AND project_type = 'amail' AND status = 1";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
 
     foreach ($res as $row) {
-        
         $user_id = $row['id'];
-        $sql = "SELECT id FROM `users` WHERE id = $user_id";
+        $refer_code = $row['refer_code'];
+        $sql = "SELECT id FROM `users` WHERE referred_by = '$refer_code'";
         $db->sql($sql);
         $res = $db->getResult();
         $num = $db->numRows($res);
-        if($num == 1){
-            $ID = $res[0]['id'];
-            $codes = 200;
-            $datetime = date('Y-m-d H:i:s');
-            $type = 'code_bonus';
-            $per_code_cost = $fn->get_code_per_cost($ID);
-            $amount = $codes * $per_code_cost;
-    
-            $sql = "INSERT INTO transactions (`user_id`,`codes`,`amount`,`datetime`,`type`)VALUES('$ID','$codes','$amount','$datetime','$type')";
-            $db->sql($sql);
-            $res = $db->getResult();
-        
-            $sql = "UPDATE `users` SET  `today_codes` = today_codes + $codes,`total_codes` = total_codes + $codes,`earn` = earn + $amount,`balance` = balance + $amount WHERE `id` = $ID";
-            $db->sql($sql);
-             $result = $db->getResult();
+        if($num >= 1){
+            foreach ($res as $row) {
+                $ID = $row['id'];
+                $sql_query = "INSERT INTO bonus_refer_bonus (user_id,refer_user_id,status,amount,datetime)VALUES($user_id,$ID,0,700,'$datetime')";
+                $db->sql($sql_query);
 
+            }
         }
 
 
     }
     $response['success'] = true;
-    $response['message'] = "refer added Bonus Done";
+    $response['message'] = "refer added";
     
     print_r(json_encode($response));
 
