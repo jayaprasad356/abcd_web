73dc779f79ec72ff1c19661deca6d2671b2b940e
AKraja777 <ashokkumar7hitter@gmail.com>
2023-09-29 12:51:56 +0530

    header.php

--- public/db-operation.php
+++ public/db-operation.php
@@ -277,6 +277,123 @@ if (isset($_POST['cancel_withdrawal']) && $_POST['cancel_withdrawal'] == 1) {
         echo "<p class='alert alert-danger'>Invalid file format! Please upload data in CSV file!</p><br>";
     }
 }
+if (isset($_POST['amail_bulk_amount']) && $_POST['amail_bulk_amount'] == 1) {
+    $count = 0;
+    $count1 = 0;
+    $error = false;
+    $filename = $_FILES["upload_file"]["tmp_name"];
+    $result = $fn->validate_image($_FILES["upload_file"], false);
+    if (!$result) {
+        $error = true;
+    }
+    if ($_FILES["upload_file"]["size"] > 0  && $error == false) {
+        $file = fopen($filename, "r");
+        while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
+            // print_r($emapData);
+            if ($count1 != 0) {
+                $w_id = trim($db->escapeString($emapData[0]));
+             
+
+                $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
+                $db->sql($sql);
+                $sql = "SELECT * FROM `withdrawals` WHERE id = $w_id ";
+                $db->sql($sql);
+                $res = $db->getResult();
+                $user_id= $res[0]['user_id'];
+                $amount= $res[0]['amount'];
+
+                $sql = "UPDATE users SET balance= balance + $amount,withdrawal = withdrawal - $amount WHERE id = $user_id";
+                $db->sql($sql);
+                $sql = "INSERT INTO transactions (user_id,amount,datetime,type) VALUES ('$user_id','$amount','$datetime','cancelled')";
+                $db->sql($sql);
+                
+            }
+
+            $count1++;
+        }
+        fclose($file);
+        // $file = fopen($filename, "r");
+        // while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
+        //     // print_r($emapData);
+        //     if ($count1 != 0) {
+        //         $emapData[0] = trim($db->escapeString($emapData[0]));
+        //         $emapData[1] = trim($db->escapeString($emapData[1]));  
+                
+        //         $sql = "UPDATE users SET `support_id`= $emapData[1],op_leads = 1 WHERE id= $emapData[0]";
+        //         $db->sql($sql);
+
+        //     }
+
+        //     $count1++;
+        // }
+        // fclose($file);
+
+        echo "<p class='alert alert-success'>CSV file is successfully imported!</p><br>";
+    } else {
+        echo "<p class='alert alert-danger'>Invalid file format! Please upload data in CSV file!</p><br>";
+    }
+}
+if (isset($_POST['abcd_bulk_codes']) && $_POST['abcd_bulk_codes'] == 1) {
+    $count = 0;
+    $count1 = 0;
+    $error = false;
+    $filename = $_FILES["upload_file"]["tmp_name"];
+    $result = $fn->validate_image($_FILES["upload_file"], false);
+    if (!$result) {
+        $error = true;
+    }
+    if ($_FILES["upload_file"]["size"] > 0  && $error == false) {
+        $file = fopen($filename, "r");
+        while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
+            // print_r($emapData);
+            if ($count1 != 0) {
+                $user_id = trim($db->escapeString($emapData[0]));
+                $codes = trim($db->escapeString($emapData[1]));
+
+                
+                $ID = $row['id'];
+                $balance = $row['amount'];
+                $sql = "SELECT id FROM `users` WHERE mobile = $mobile";
+                $db->sql($sql);
+                $res = $db->getResult();
+                $num = $db->numRows($res);
+                if($num == 1){
+                    $ID = $row['id'];
+                    
+                    $datetime = date('Y-m-d H:i:s');
+                    $type = 'admin_credit_balance';
+                    $sql = "INSERT INTO transactions (`user_id`,`amount`,`datetime`,`type`)VALUES('$ID','$balance','$datetime','$type')";
+                    $db->sql($sql);
+                    $sql_query = "UPDATE users SET balance=balance+ $balance WHERE id=$ID";
+                    $db->sql($sql_query);
+                    $result = $db->getResult();
+            }
+        }
+
+            $count1++;
+        }
+        fclose($file);
+        // $file = fopen($filename, "r");
+        // while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
+        //     // print_r($emapData);
+        //     if ($count1 != 0) {
+        //         $emapData[0] = trim($db->escapeString($emapData[0]));
+        //         $emapData[1] = trim($db->escapeString($emapData[1]));  
+                
+        //         $sql = "UPDATE users SET `support_id`= $emapData[1],op_leads = 1 WHERE id= $emapData[0]";
+        //         $db->sql($sql);
+
+        //     }
+
+        //     $count1++;
+        // }
+        // fclose($file);
+
+        echo "<p class='alert alert-success'>CSV file is successfully imported!</p><br>";
+    } else {
+        echo "<p class='alert alert-danger'>Invalid file format! Please upload data in CSV file!</p><br>";
+    }
+}
 if (isset($_POST['bulk_suppoort_change']) && $_POST['bulk_suppoort_change'] == 1) {
     $count = 0;
     $count1 = 0;
