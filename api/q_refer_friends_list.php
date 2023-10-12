bb44b1777ea2c3099cc4e715d562ec2176465450
AKraja777 <ashokkumar7hitter@gmail.com>
2023-10-12 19:09:22 +0530

    q_refer_friends_list.php

--- api/q_refer_friends_list.php
+++ api/q_refer_friends_list.php
@@ -6,38 +6,41 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-date_default_timezone_set('Asia/Kolkata');
 
 include_once('../includes/crud.php');
+
 $db = new Database();
 $db->connect();
 
 if (empty($_POST['user_id'])) {
     $response['success'] = false;
-    $response['message'] = " User Id is Empty";
+    $response['message'] = "User Id is Empty";
     print_r(json_encode($response));
     return false;
 }
-
 $user_id = $db->escapeString($_POST['user_id']);
 
-$sql = "SELECT * FROM `refer_friends` WHERE id = $user_id";
+$sql = "SELECT * FROM refer_friends WHERE user_id = $user_id";
 $db->sql($sql);
-$res = $db->getResult();
+$res= $db->getResult();
 $num = $db->numRows($res);
-if($num>=1){
- 
+
+if ($num >= 1){
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['user_id'] = $row['user_id'];
+        $temp['friend_mobile'] = $row['friend_mobile'];
+        $temp['description'] = $row['description'];
+        $rows[] = $temp;
+    }
     $response['success'] = true;
     $response['message'] = "Refer Friends Listed Successfully";
-    $response['data'] = $res;
+    $response['data'] = $rows;
     print_r(json_encode($response));
-
 }
 else{
     $response['success'] = false;
-    $response['message'] = "Data Not Found";
+    $response['message'] = "Data Not found";
     print_r(json_encode($response));
+
 }
-
-
-?>
