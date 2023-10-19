b24cc33d9d44e1447fdceabaa86fdb08216d6b17
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-19 13:41:49 +0530

    reverse

--- test/reverse_mw.php
+++ test/reverse_mw.php
@@ -15,6 +15,8 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
+
+
 $sql = "SELECT u.id,t.id AS t_id,u.mobile,u.name,t.amount,u.worked_days,u.duration,u.monthly_wallet FROM `users`u,`transactions`t WHERE u.id = t.user_id AND u.level = 1 AND u.plan = 50 AND worked_days < 50 AND t.type = 'monthly_wallet' AND DATE(t.datetime) = '2023-10-18' AND u.id != 58467";
 $db->sql($sql);
 $res = $db->getResult();
@@ -24,24 +26,19 @@ if ($num >= 1) {
     foreach ($res as $row) {
         $user_id = $row['id'];
         $amount = $row['amount'];
-        $sql = "UPDATE users SET balance= balance + $amount,earn = earn + $amount,monthly_wallet = monthly_wallet - $monthly_wallet,old_monthly_wallet = 0,monthly_wallet_status = 0 WHERE id=" . $user_id;
+        $amount = -$amount;
+        $type = 'admin_credit_balance';
+        $sql = "INSERT INTO transactions (`user_id`,`amount`,`datetime`,`type`)VALUES('$user_id','$amount','$datetime','$type')";
         $db->sql($sql);
-        $sql = "SELECT id FROM `users` WHERE referred_by = '$refer_code' AND status = 1 ORDER BY joined_date DESC LIMIT $current_refers";
+
+        $sql = "UPDATE users SET balance= balance + $amount,earn = earn + $amount,monthly_wallet = monthly_wallet - $amount,monthly_wallet_status = 1 WHERE id=" . $user_id;
         $db->sql($sql);
-        $res = $db->getResult();
-        $num = $db->numRows($res);
-        if($num >= 1){
-            foreach ($res as $row) {
-                $ID = $row['id'];
-
-
-            }
-        }
+        
 
 
     }
     $response['success'] = true;
-    $response['message'] = "refer added";
+    $response['message'] = "reverse added";
     
     print_r(json_encode($response));
 
