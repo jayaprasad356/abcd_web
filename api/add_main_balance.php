0ffb1ab9b243f3baef6977b547e8df4e86979b2c
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-19 11:04:51 +0530

    main

--- api/add_main_balance.php
+++ api/add_main_balance.php
@@ -145,12 +145,30 @@ if ($num == 1) {
                 $db->sql($sql);
                 $sql = "UPDATE users SET balance= balance + $monthly_wallet,earn = earn + $monthly_wallet,monthly_wallet = monthly_wallet - $monthly_wallet,old_monthly_wallet = 0,monthly_wallet_status = 0 WHERE id=" . $user_id;
                 $db->sql($sql);
+
+                $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
+                $db->sql($sql);
+                $res = $db->getResult();
+                $response['success'] = true;
+                $response['message'] = "Added to Main Balance Successfully";
+                $response['data'] = $res;
+                print_r(json_encode($response));
+                return false;
             }else{
                 if($level > 1){
                     $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'monthly_wallet','$datetime',$monthly_wallet)";
                     $db->sql($sql);
                     $sql = "UPDATE users SET balance= balance + monthly_wallet,earn = earn + monthly_wallet,monthly_wallet = 0 WHERE id=" . $user_id;
                     $db->sql($sql);
+
+                    $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
+                    $db->sql($sql);
+                    $res = $db->getResult();
+                    $response['success'] = true;
+                    $response['message'] = "Added to Main Balance Successfully";
+                    $response['data'] = $res;
+                    print_r(json_encode($response));
+                    return false;
                 }else{
                     $response['success'] = false;
                     $response['message'] = "Complete 50 Days To Withdraw";
