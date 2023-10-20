68facc1fb91e1139c24e360c1a37f40762dce816
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-21 00:56:50 +0530

    reverse

--- test/reverse_mw.php
+++ test/reverse_mw.php
@@ -17,10 +17,10 @@ $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
 
 
-$sql = "SELECT u.id,u.mobile,u.name,u.monthly_wallet,u.level,u.worked_days,u.duration,t.amount,t.datetime 
+$sql = "SELECT u.id,u.mobile,u.name,u.monthly_wallet,u.level,u.worked_days,u.duration,t.amount,t.datetime ,u.balance
 FROM `users` u, `transactions` t 
 WHERE u.id = t.user_id 
-AND DATE(t.datetime) >= '2023-10-19' AND DATE(t.datetime) <= '2023-10-20' AND t.type = 'monthly_wallet' AND u.plan = 50 AND u.level <= 2 AND .u.worked_days < u.duration";
+AND DATE(t.datetime) >= '2023-10-19' AND DATE(t.datetime) <= '2023-10-20' AND t.type = 'monthly_wallet' AND u.plan = 50 AND u.level <= 2 AND .u.worked_days < u.duration AND u.balance < 0";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
@@ -29,12 +29,12 @@ if ($num >= 1) {
     foreach ($res as $row) {
         $user_id = $row['id'];
         $amount = $row['amount'];
-        $amount = -$amount;
+        //$amount = -$amount;
         $type = 'admin_credit_balance';
         $sql = "INSERT INTO transactions (`user_id`,`amount`,`datetime`,`type`)VALUES('$user_id','$amount','$datetime','$type')";
         $db->sql($sql);
 
-        $sql = "UPDATE users SET balance= balance + $amount,earn = earn + $amount,monthly_wallet = monthly_wallet - $amount,monthly_wallet_status = 1 WHERE id=" . $user_id;
+        $sql = "UPDATE users SET balance= balance + $amount,earn = earn + $amount,monthly_wallet = monthly_wallet - $amount,monthly_wallet_status = 0 WHERE id=" . $user_id;
         $db->sql($sql);
         
 
