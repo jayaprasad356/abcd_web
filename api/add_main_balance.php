0a25c349f5fda9506362dac9e349d62b7ea74a37
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-09 13:35:43 +0530

    add

--- api/add_main_balance.php
+++ api/add_main_balance.php
@@ -145,7 +145,7 @@ if ($num == 1) {
                         $monthly_wallet = $old_monthly_wallet + $result;
                         $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'monthly_wallet','$datetime',$monthly_wallet)";
                         $db->sql($sql);
-                        $sql = "UPDATE users SET balance= balance + $monthly_wallet,earn = earn + $monthly_wallet,monthly_wallet = monthly_wallet - $monthly_wallet,old_monthly_wallet = 0,monthly_wallet_status = 0 WHERE id=" . $user_id;
+                        $sql = "UPDATE users SET balance= balance + $monthly_wallet,earn = earn + $monthly_wallet,monthly_wallet = 0,old_monthly_wallet = 0,monthly_wallet_status = 0 WHERE id=" . $user_id;
                         $db->sql($sql);
                         $response['success'] = true;
                         $response['message'] = "Added to Main Balance Successfully".$old_monthly_wallet;
