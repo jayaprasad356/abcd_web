2b3a19a481dd1c7fdeed6e2f922f39364e659faf
jayaprasad356 <jayaprasad356@gmail.com>
2023-12-21 00:51:00 +0530

    cancel

--- test/cancel_withdrawal.php
+++ test/cancel_withdrawal.php
@@ -15,24 +15,23 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
-$sql = "SELECT * FROM `withdrawals` WHERE status = 0";
+$sql = "SELECT * FROM `withdrawals` WHERE status = 0 AND user_id = 43548";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
 
     foreach ($res as $row) {
-        $w_id = $row['w_id'];
+        $w_id = $row['id'];
+        $user_id= $row['user_id'];
+        $amount= $row['amount'];
         $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
         $db->sql($sql);
-        $sql = "SELECT * FROM `withdrawals` WHERE id = $w_id ";
-        $db->sql($sql);
-        $res = $db->getResult();
-        $user_id= $res[0]['user_id'];
-        $amount= $res[0]['amount'];
 
-        $sql = "UPDATE users SET balance= balance - $amount,withdrawal = withdrawal + $amount WHERE id = $user_id";
+
+        $sql = "UPDATE users SET balance= balance + $amount,withdrawal = withdrawal - $amount WHERE id = $user_id";
         $db->sql($sql);
+        
         $sql = "INSERT INTO transactions (user_id,amount,datetime,type) VALUES ('$user_id','$amount','$datetime','cancelled')";
         $db->sql($sql);
 
